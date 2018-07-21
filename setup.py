990d7009a884f230dc8dbc4c9aa9e0e4a1f8f105
Jan Clemens <clemensjan@googlemail.com>
2018-07-21 18:18:22 +0200

    prepare github

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='defoptcfg',
+    version='0.1',
+    py_modules=['defoptcfg'],
+    # test_suite='test_defopt',
+    install_requires=[
+        'docutils',
+        'sphinxcontrib-napoleon>=0.5.1',
+    ],
+    extras_require={
+        ':sys.platform=="win32"': ['colorama>=0.3.4'],
+    },
+    keywords='argument parser parsing optparse argparse getopt docopt sphinx configargparse',
+)
